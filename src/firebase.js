8952676d253f0673b312e7c955c6aeb27e832b57
aymenhattabi14-gif <aymenhattabi14@gmail.com>
2026-07-22 12:34:45 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC40-dwjYoDbPAbxFrtvVhoB1snpC4F6yc",
-  authDomain: "scale-up-nano.firebaseapp.com",
-  projectId: "scale-up-nano",
-  storageBucket: "scale-up-nano.firebasestorage.app",
-  messagingSenderId: "1075963546243",
-  appId: "1:1075963546243:web:7e0921d622fad9694c29b6",
-  measurementId: "G-FFVB7K9FT1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+{
+  "name": "scale-up-nano",
+  "private": true,
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "firebase": "^10.12.0",
+    "lucide-react": "0.383.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0"
+  },
+  "devDependencies": {
+    "@vitejs/plugin-react": "^4.2.1",
+    "vite": "^5.2.0"
+  }
+}
